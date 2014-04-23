07c09406f595fdaa7ac1a8d298a1efaa5ccb43bc
Aleksey Zolotov <myxrome@gmail.com>
2014-04-23 11:01:51 +0400

    удаление избыточной связи между Темой и Категорией. Категория принадлежит 1 Теме ref #7

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,5 +1,5 @@
 class Topic < ActiveRecord::Base
-  has_many :categories, -> { order 'categories.order' }, inverse_of: :topics, dependent: :destroy
+  has_many :categories, -> { order 'categories.order' }, inverse_of: :topic, dependent: :destroy
   has_many :values, -> { where('values.active = true and values.end_date > ?', DateTime.now.to_date) },
            through: :categories
   after_initialize :init_key
