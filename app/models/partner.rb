6a31b8ca10b4de706e903e385ed28e11f50f7e43
Aleksey Zolotov <myxrome@gmail.com>
2014-05-19 20:22:22 +0400

    общая папка для вложений. resolve #10

--- app/models/partner.rb
+++ app/models/partner.rb
@@ -1,6 +1,6 @@
 class Partner < ActiveRecord::Base
 
-  has_attached_file :logo, url: '/l/:id/:hash.:extension', hash_secret: '933QPmmE2uEvGULxSw4jvWEh'
+  has_attached_file :logo, url: '/content/l/:id/:hash.:extension', hash_secret: '933QPmmE2uEvGULxSw4jvWEh'
   validates_attachment_content_type :logo, :content_type => %w(image/jpeg image/jpg image/png)
 
 end
--- app/models/promo.rb
+++ app/models/promo.rb
@@ -6,7 +6,7 @@ class Promo < ActiveRecord::Base
   end
 
   has_attached_file :image, styles: {xhdpi: ['100%', :png], hdpi: ['75%', :png], mdpi: ['50%', :png], ldpi: ['37%', :png], thumb: ['100x100', :png]},
-                    default_style: :xhdpi, url: '/p/:value_id/:id/:style/:hash.:extension',
+                    default_style: :xhdpi, url: '/content/p/:value_id/:id/:style/:hash.:extension',
                     hash_secret: '933QPmmE2uEvGULxSw4jvWEh'
   validates_attachment_content_type :image, :content_type => %w(image/jpeg image/jpg image/png)
 
