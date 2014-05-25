deea0157f608c99dd0388f2bd6657c5953651930
Aleksey Zolotov <myxrome@gmail.com>
2014-05-25 12:45:11 +0400

    каскадные изменения Категории и Темы. resolve #14

--- app/models/value.rb
+++ app/models/value.rb
@@ -1,5 +1,5 @@
 class Value < ActiveRecord::Base
-  belongs_to :category, inverse_of: :values
+  belongs_to :category, touch: true, inverse_of: :values
   has_many :descriptions, -> { order 'descriptions.order' }, inverse_of: :value, dependent: :destroy
   accepts_nested_attributes_for :descriptions, reject_if: :all_blank, allow_destroy: true
   has_many :promos, -> { order 'promos.order' }, inverse_of: :value, dependent: :destroy
