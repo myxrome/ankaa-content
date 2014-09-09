83fec149b766bdf68935282ced950fb2b825aa7f
Aleksey Zolotov <myxrome@gmail.com>
2014-09-09 16:28:47 +0400

    логика сортировки и пересортировки добвлена в модель. ref #16

--- app/models/category.rb
+++ app/models/category.rb
@@ -5,5 +5,31 @@ class Category < ActiveRecord::Base
   belongs_to :topic, touch: true, inverse_of: :categories
   has_many :values, -> { where active: true }, inverse_of: :category, dependent: :destroy
   has_many :facts, as: :context
+  after_create :reorder
+
+  def reorder
+    update_attribute(:order, (Category.where('categories.topic_id = ?', self.topic_id).
+        maximum(:order) || 0) + 1) unless self.order
+  end
+
+  def move_up
+    upper = Category.where('categories.topic_id = ? AND categories.order < ?',
+                           self.topic_id, self.order).order(order: :desc).limit(1).first
+    if upper
+      low = upper.order
+      upper.update_attribute(:order, self.order)
+      update_attribute(:order, low)
+    end
+  end
+
+  def move_down
+    lower = Category.where('categories.topic_id = ? AND categories.order > ?',
+                           self.topic_id, self.order).order(order: :asc).limit(1).first
+    if lower
+      up = lower.order
+      lower.update_attribute(:order, self.order)
+      update_attribute(:order, up)
+    end
+  end
 
 end
--- app/models/topic.rb
+++ app/models/topic.rb
@@ -5,9 +5,32 @@ class Topic < ActiveRecord::Base
   has_many :values, through: :categories
   has_many :facts, as: :context
   after_initialize :init_key
+  after_create :reorder
 
   def init_key
     self.key ||= SecureRandom.hex
+  end
+
+  def reorder
+    update_attribute(:order, (Topic.maximum(:order) || 0) + 1) unless self.order
+  end
+
+  def move_up
+    upper = Topic.where('topics.order < ?', self.order).order(order: :desc).limit(1).first
+    if upper
+      low = upper.order
+      upper.update_attribute(:order, self.order)
+      update_attribute(:order, low)
+    end
+  end
+
+  def move_down
+    lower = Topic.where('topics.order > ?', self.order).order(order: :asc).limit(1).first
+    if lower
+      up = lower.order
+      lower.update_attribute(:order, self.order)
+      update_attribute(:order, up)
+    end
   end
 
 end
