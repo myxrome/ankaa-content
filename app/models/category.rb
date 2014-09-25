f87e535f4d804d5ae90d0d99ae94cff236aecd83
Aleksey Zolotov <myxrome@gmail.com>
2014-09-25 11:39:39 +0400

    рефакторинг сортировки. вынесено в отдельный модуль. resolve #20

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,31 +1,15 @@
 class Category < ActiveRecord::Base
+  require 'concerns/orderable'
+  include Orderable
+
+  scope :neighbors, -> (category) { where(topic_id: category.topic_id) }
+
+  def neighbors
+    Category.neighbors(self)
+  end
+
   belongs_to :topic, touch: true, inverse_of: :categories
   has_many :values, -> { where(active: true).order(:name) }, inverse_of: :category, dependent: :destroy
   has_many :facts, as: :context
-  after_create :reorder
-
-  def reorder
-    update_attribute(:order, (Category.where('categories.topic_id = ?', self.topic_id).
-        maximum(:order) || 0) + 1) unless self.order
-  end
-
-  def move_up
-    upper = Category.where('categories.topic_id = ? AND categories.order < ?',
-                           self.topic_id, self.order).order(order: :desc).limit(1).first
-    switch_order(upper) if upper
-  end
-
-  def move_down
-    lower = Category.where('categories.topic_id = ? AND categories.order > ?',
-                           self.topic_id, self.order).order(order: :asc).limit(1).first
-    switch_order(lower) if lower
-  end
-
-  private
-  def switch_order(other)
-    low = other.order
-    other.update_attribute(:order, self.order)
-    update_attribute(:order, low)
-  end
 
 end
--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,32 +1,19 @@
 class Topic < ActiveRecord::Base
+  require 'concerns/orderable'
+  include Orderable
+
+  scope :neighbors, -> (topic) { all.order(:order) }
+
+  def neighbors
+    Topic.neighbors(self)
+  end
+
   has_many :categories, -> { where(active: true).order(:order) }, inverse_of: :topic, dependent: :destroy
   has_many :facts, as: :context
   after_initialize :init_key
-  after_create :reorder
 
   def init_key
     self.key ||= SecureRandom.hex
-  end
-
-  def reorder
-    update_attribute(:order, (Topic.maximum(:order) || 0) + 1) unless self.order
-  end
-
-  def move_up
-    upper = Topic.where('topics.order < ?', self.order).order(order: :desc).limit(1).first
-    switch_order(upper) if upper
-  end
-
-  def move_down
-    lower = Topic.where('topics.order > ?', self.order).order(order: :asc).limit(1).first
-    switch_order(lower) if lower
-  end
-
-  private
-  def switch_order(other)
-    low = other.order
-    other.update_attribute(:order, self.order)
-    update_attribute(:order, low)
   end
 
 end
