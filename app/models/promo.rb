7ac74a3b373fb42f3b7b3b3434e057ceb8bf3df3
myxrome <myxrome@myxrome-nb>
2014-10-28 22:19:33 +0300

    фикс ошибки. ref #25

--- app/models/promo.rb
+++ app/models/promo.rb
@@ -7,11 +7,11 @@ class Promo < ActiveRecord::Base
 
   has_attached_file :image, styles: {
       xhdpi: {geometry: '100%', quality: 92, format: :jpg },
-      hdpi: {geometry: '75%', quality: 92, format: :jpg }
-      mdpi: {geometry: '50%', quality: 92, format: :jpg }
-      ldpi: {geometry: '37%', quality: 92, format: :jpg }
+      hdpi: {geometry: '75%', quality: 92, format: :jpg },
+      mdpi: {geometry: '50%', quality: 92, format: :jpg },
+      ldpi: {geometry: '37%', quality: 92, format: :jpg },
       thumb: {geometry: '100x100', quality: 92, format: :jpg }
-                    default_style: :xhdpi, url: '/content/p/:value_id/:id/:style/:hash.:extension',
+  }, default_style: :xhdpi, url: '/content/p/:value_id/:id/:style/:hash.:extension',
                     hash_secret: '933QPmmE2uEvGULxSw4jvWEh'
   validates_attachment_content_type :image, :content_type => %w(image/jpeg image/jpg image/png)
 
