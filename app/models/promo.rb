4291c0e652396921c2af8a50b49e00ce23502803
Aleksey Zolotov <myxrome@gmail.com>
2015-03-16 09:31:57 +0300

    ошибка автоматического подсчета поля скидки исправлена. resolve #44

--- app/models/promo.rb
+++ app/models/promo.rb
@@ -9,12 +9,15 @@ class Promo < ActiveRecord::Base
 
   has_attached_file :image,
                     styles: {
-                        x100: {geometry: '100x100', quality: 92, format: :jpg},
-                        x200: {geometry: '200x200', quality: 92, format: :jpg},
-                        x300: {geometry: '300x300', quality: 92, format: :jpg},
-                        x400: {geometry: '400x400', quality: 92, format: :jpg},
-                        x500: {geometry: '500x500', quality: 92, format: :jpg},
-                        x600: {geometry: '600x600', quality: 92, format: :jpg}
+                        x100: {geometry: '200x200', quality: 92, format: :jpg},
+                        x200: {geometry: '400x400', quality: 92, format: :jpg},
+                        x300: {geometry: '600x600', quality: 92, format: :jpg},
+                        x400: {geometry: '800x800', quality: 92, format: :jpg},
+                        x500: {geometry: '1000x1000', quality: 92, format: :jpg},
+                        x600: {geometry: '1200x1200', quality: 92, format: :jpg},
+                        x700: {geometry: '1400x1400', quality: 92, format: :jpg},
+                        x800: {geometry: '1600x1600', quality: 92, format: :jpg},
+                        x900: {geometry: '1800x1800', quality: 92, format: :jpg},
                     },
                     default_style: :x300,
                     url: '/content/v/:value_id/promo/:style/:id:file_key.:extension'
--- app/models/value.rb
+++ app/models/value.rb
@@ -11,12 +11,15 @@ class Value < ActiveRecord::Base
 
   has_attached_file :thumb,
                     styles: {
-                        x100: {geometry: '50x50', quality: 92, format: :jpg},
-                        x200: {geometry: '100x100', quality: 92, format: :jpg},
-                        x300: {geometry: '150x150', quality: 92, format: :jpg},
-                        x400: {geometry: '200x200', quality: 92, format: :jpg},
-                        x500: {geometry: '250x250', quality: 92, format: :jpg},
-                        x600: {geometry: '300x300', quality: 92, format: :jpg}
+                        x100: {geometry: '100x100', quality: 92, format: :jpg},
+                        x200: {geometry: '200x200', quality: 92, format: :jpg},
+                        x300: {geometry: '300x300', quality: 92, format: :jpg},
+                        x400: {geometry: '400x400', quality: 92, format: :jpg},
+                        x500: {geometry: '500x500', quality: 92, format: :jpg},
+                        x600: {geometry: '600x600', quality: 92, format: :jpg},
+                        x700: {geometry: '700x700', quality: 92, format: :jpg},
+                        x800: {geometry: '800x800', quality: 92, format: :jpg},
+                        x900: {geometry: '900x900', quality: 92, format: :jpg},
                     },
                     default_style: :x300,
                     url: '/content/v/:id/thumb/:style/:file_key.:extension'
